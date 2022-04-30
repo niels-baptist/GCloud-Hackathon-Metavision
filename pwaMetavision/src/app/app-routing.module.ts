90006e8c4d2cffedb83d36bd4ab265ebb5417ff3
Niels baptist <nielsbaptist01@gmail.com>
2022-04-30 17:05:04 +0200

    dashboard page

--- pwaMetavision/src/app/app-routing.module.ts
+++ pwaMetavision/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
 import { SetupComponent } from './setup/setup.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'setup', component: SetupComponent },
+  { path: 'dashboard', component: DashboardComponent },
 ];
 
 @NgModule({
